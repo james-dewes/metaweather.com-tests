cce8c6238f4ad17cf67f67b875f47baee9e1b4e6
James Dewes <james-dewes@users.noreply.github.com>
2019-10-05 18:27:09 +0100

    Re-arranges the tests to make multiple calls and test each attribute separately

--- cypress/integration/location-tomorrow.js
+++ cypress/integration/location-tomorrow.js
@@ -1,5 +1,14 @@
 /// <reference types="Cypress" />
 
+/* Re-arranges the tests to make multiple calls and test
+ * each attribute separately, checking every object in
+ * the response.
+ * 
+ * NB this set of tests makes multiple requests to the
+ * API and you may need to check with the owner for
+ * permission to do so.
+ */
+
 function Tomorrow(){
 
   let today = new Date();
@@ -78,69 +87,155 @@ let compassPoints = [
 ]
 
 locations.forEach(function(locaton){
-  describe('Test metaweather.com for ' + locaton.name, function() {
-    it('Tests the API for Nottingham on ' + tomorrow.getDate('/'), function() {
-      cy.request( '/api/location/' + locaton.woeid + '/' + tomorrow.getDate('/')).then((response) => {
 
-        //expect a good response
-        expect(response.status).to.equal(200) 
+  let titleString = 'Test for ' + locaton.name +' ' + tomorrow.getDate('/');
+  let url = '/api/location/' + locaton.woeid + '/' + tomorrow.getDate('/');
 
-        //expect the body not to be empty
+  describe(titleString, function() {
+    it('Expect a good response', function() {
+      cy.request(url).then((response) => {
+        expect(response.status).to.equal(200);
+      });
+    });
+
+    it('Expect a response that is not empty', function() {
+      cy.request(url).then((response) => {
         expect(response.body).to.be.an('array').that.is.not.empty; 
+      });
+    });
 
-        //set an initial date for the date ordered test
-        let last = Date.now(); 
+    it('Expect weather to be an object', function() {
+      cy.request(url).then((response) => {
+        cy.wrap(response.body).each(function(value, index){
+          let weather = response.body[index];
+          expect(weather).to.be.an('Object');
+        });
+      });
+    });
 
-        cy.wrap(response.body).each(function(value, index){ //for each element of the response
-            let weather = response.body[index];
+    it('Expect response to be date ordered, newest first', function() {
+      //set an initial date for the date ordered test
+      let last = Date.now(); 
+      cy.request(url).then((response) => {
+        cy.wrap(response.body).each(function(value, index){
+          let weather = response.body[index];
+          //date ordered, newest first
+          expect(new Date(weather.created).valueOf()).to.be.below(last); 
+          last = weather.created;
+        });
+      });
+    });
 
-            expect(weather).to.be.an('Object');
+    it('Expect the applicable date to match the searched for date ', function() {
+      cy.request(url).then((response) => {
+        cy.wrap(response.body).each(function(value, index){
+          let weather = response.body[index];
+          expect(weather.applicable_date).to.equal(tomorrow.getDate('-')); 
+        });
+      });
+    });
 
-            //date ordered, newest first
-            expect(new Date(weather.created).valueOf()).to.be.below(last); 
-            last = weather.created;
+    it('Expect the abbreviation to be in the keys of the weatherState object', function() {
+      cy.request(url).then((response) => {
+        cy.wrap(response.body).each(function(value, index){
+          let weather = response.body[index];
+        expect(weather.weather_state_abbr).to.have.oneOf(Object.keys(weatherState)); 
+        });
+      });
+    });
 
-            //expect the applicable date to match the searched for date
-            expect(weather.applicable_date).to.equal(tomorrow.getDate('-')); 
+    it('Expect weather_state_name to relate to the weather_state_abbr', function() {
+      cy.request(url).then((response) => {
+        cy.wrap(response.body).each(function(value, index){
+          let weather = response.body[index];
+          expect(weather.weather_state_name).to.equal(weatherState[weather.weather_state_abbr]); 
+        });
+      });
+    });
 
-            //expect the abbreviation to be in the keys of the weatherState object
-            expect(weather.weather_state_abbr).to.have.oneOf(Object.keys(weatherState)); 
+    it('Expect the wind_speed to be non-negative', function() {
+      cy.request(url).then((response) => {
+        cy.wrap(response.body).each(function(value, index){
+          let weather = response.body[index];
+          expect(weather.wind_speed).to.be.least(0);
+        });
+      });
+    });
 
-            //expect weather_state_name to relate to the weather_state_abbr
-            expect(weather.weather_state_name).to.equal(weatherState[weather.weather_state_abbr]); 
-            
-            //expect the wind_speed to be non-negative
-            expect(weather.wind_speed).to.be.least(0);
-            
-            //expect the wind_direction to be a valid compass reading within (1-360)
-            expect(weather.wind_direction).to.be.within(1,360);
+    it('Expect the wind_direction to be a valid compass reading within (1-360)', function() {
+      cy.request(url).then((response) => {
+        cy.wrap(response.body).each(function(value, index){
+          let weather = response.body[index];
+          expect(weather.wind_direction).to.be.within(1,360);
+        });
+      });
+    });
 
-            //expect the wind_direction_compass to be a valid compass point value
-            expect(weather.wind_direction_compass).to.be.oneOf(compassPoints);
-            //TODO - calculate expected value from the wind_direction and check that
-            
-            //max_temp >= min_temp 
-            expect(weather.max_temp).to.be.least(weather.min_temp);
-            
-            //min_temp <= the_temp <= max_temp 
-            expect(weather.the_temp).to.be.within(weather.min_temp,weather.max_temp);
-            
-            //air_pressure
-            expect(weather.air_pressure).to.be.an.number;
-            //humidity
-            expect(weather.humidity).to.be.an.number;
-            //visibility
-            expect(weather.visibility).to.be.an.number;
+    it('Expect the wind_direction_compass to be a valid compass point value', function() {
+      cy.request(url).then((response) => {
+        cy.wrap(response.body).each(function(value, index){
+          let weather = response.body[index];
+          expect(weather.wind_direction_compass).to.be.oneOf(compassPoints);      
+        });
+      });
+    });
 
-            //expect predictability to be within 0 (no agreement) and 100 (total agreement)
-            expect(weather.predictability).to.be.within(0,100);
-             
+    //TODO - calculate expected value from the wind_direction and check that
+
+    it('Expect max_temp >= min_temp', function() {
+      cy.request(url).then((response) => {
+        cy.wrap(response.body).each(function(value, index){
+          let weather = response.body[index];
+            expect(weather.max_temp).to.be.least(weather.min_temp);          
+        });
+      });
+    });
+
+    it('Expect min_temp <= the_temp <= max_temp', function() {
+      cy.request(url).then((response) => {
+        cy.wrap(response.body).each(function(value, index){
+          let weather = response.body[index];          
+          expect(weather.the_temp).to.be.within(weather.min_temp,weather.max_temp);
+        });
+      });
+    });
+
+    it('Expect air_pressure to be a number', function() {
+      cy.request(url).then((response) => {
+        cy.wrap(response.body).each(function(value, index){
+          let weather = response.body[index];
+          expect(weather.air_pressure).to.be.an.number;
+        });
+      });
+    });
+      
+    it('Expect humidity ro be a number', function() {
+      cy.request(url).then((response) => {
+        cy.wrap(response.body).each(function(value, index){
+          let weather = response.body[index];
+          expect(weather.humidity).to.be.an.number;
+        });
+      });
+    });
+      
+    it('Expect visibility to be a number', function() {
+      cy.request(url).then((response) => {
+        cy.wrap(response.body).each(function(value, index){
+          let weather = response.body[index];
+          expect(weather.visibility).to.be.an.number;
+        });
+      });
+    });
+    
+    it('Expect predictability to be within 0 (no agreement) and 100 (total agreement)', function() {
+      cy.request(url).then((response) => {
+        cy.wrap(response.body).each(function(value, index){
+          let weather = response.body[index];
+          expect(weather.predictability).to.be.within(0,100);  
         });
       });
     });
   });
 });
-
-
 
  
